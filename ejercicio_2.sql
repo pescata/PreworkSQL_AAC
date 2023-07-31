48b342756b82f21a03c8846214903d333bb8e211
pescata <aalvarez_cid@hotmail.com>
2023-07-31 11:15:15 +0200

    feat: updated 31/07/2023
    Ejercicio 2 completado

--- ejercicio_2.sql
+++ ejercicio_2.sql
@@ -38,11 +38,40 @@ WHERE id = 1
 /*
 1. Crea una tabla llamada "Ciudades" con las columnas: "id" (entero, clave
 primaria), "nombre" (texto) y "pais" (texto).
+*/
+CREATE TABLE IF  NOT EXISTS public.ciudades (
+	id INT PRIMARY KEY,
+	nombre VARCHAR(255),
+	pais VARCHAR(255)
+)
+/*
 2. Inserta al menos tres registros en la tabla "Ciudades".
+*/
+INSERT INTO public.ciudades (id,nombre,pais)
+VALUES 
+(1,'vigo','españa'),
+(2,'cape town','sudáfrica'),
+(3,'londres','inglaterra')
+/*
 3. Crea una foreign key en la tabla "Usuarios" que se relacione con la columna "id"
 de la tabla "Ciudades".
+*/
+ALTER TABLE public.usuarios
+ADD id_ciudad INT
+
+ALTER TABLE public.usuarios
+ADD CONSTRAINT FK_ciudad_id FOREIGN KEY (id_ciudad) REFERENCES public.ciudades(id)
+/*
 4. Realiza una consulta que muestre los nombres de los usuarios junto con el
 nombre de su ciudad y país (utiliza un LEFT JOIN).
+*/
+SELECT u.nombre as usuario, c.nombre as ciudad, c.pais FROM public.usuarios u
+LEFT JOIN public.ciudades c
+ON u.id_ciudad = c.id
+/*
 5. Realiza una consulta que muestre solo los usuarios que tienen una ciudad
 asociada (utiliza un INNER JOIN).
 */
+SELECT u.nombre as usuario, c.nombre as ciudad, c.pais FROM public.usuarios u
+INNER JOIN public.ciudades c
+ON u.id_ciudad = c.id
